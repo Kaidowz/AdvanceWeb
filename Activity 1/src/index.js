064395a84701198b91b9d1f72916d76638e94c63
Kaidowz <rafaelferalix.osorio@gmail.com>
2022-02-17 20:47:33 +0800

    first commit

---
+++ Activity 1/src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Welcome to Sandbox, Rafael Osorio!</h1>
+<div>
+<p>
+  This text is coded at codesandbox. You can check the website
+  <a href="https://codesandbox.io/" target="_blank" rel="noopener noreferrer">here</a>.
+</p>
+</div>
+`;
